8b56ac414aac84b5ea245e1da6421c55b6ae8f5c
Colin Ho <colinho@Colins-MacBook-Pro.local>
2024-09-15 22:35:44 -0700

    fix

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, send_from_directory
 from flask_mail import Mail,Message
-from flask_limiter import Limiter
-from flask_limiter.util import get_remote_address
+#from flask_limiter import Limiter
+#from flask_limiter.util import get_remote_address
 import os
-from langchain.vectorstores import Pinecone
-from langchain.embeddings.openai import OpenAIEmbeddings
-import pinecone
-from gptchat import get_chain
+#from langchain.vectorstores import Pinecone
+#from langchain.embeddings.openai import OpenAIEmbeddings
+#import pinecone
+#from gptchat import get_chain
 
-import gspread
-from datetime import datetime
+#import gspread
+#from datetime import datetime
 
 credentials = {
   "type": "service_account",
@@ -25,21 +25,21 @@ credentials = {
   "universe_domain": "googleapis.com"
 }
 
-scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-client = gspread.service_account_from_dict(credentials,scope)
-sheet = client.open("Chatbot Data").sheet1   
+#scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+#client = gspread.service_account_from_dict(credentials,scope)
+#sheet = client.open("Chatbot Data").sheet1   
 
-pinecone.init(api_key=os.environ.get('PINECONE_API_KEY'), environment=os.environ.get('PINECONE_ENVIRONMENT'))
-embeddings = OpenAIEmbeddings()
-vectorstore = Pinecone.from_existing_index(index_name=os.environ.get('PINECONE_INDEX_NAME'), embedding=embeddings, namespace=os.environ.get('PINECONE_NAMESPACE'))
+# pinecone.init(api_key=os.environ.get('PINECONE_API_KEY'), environment=os.environ.get('PINECONE_ENVIRONMENT'))
+# embeddings = OpenAIEmbeddings()
+# vectorstore = Pinecone.from_existing_index(index_name=os.environ.get('PINECONE_INDEX_NAME'), embedding=embeddings, namespace=os.environ.get('PINECONE_NAMESPACE'))
 
 app = Flask(__name__,static_url_path='', static_folder='build')
-limiter = Limiter(
-    get_remote_address,
-    app=app,
-    default_limits=["200 per day", "50 per hour"],
-    storage_uri="memory://",
-)
+# limiter = Limiter(
+#     get_remote_address,
+#     app=app,
+#     default_limits=["200 per day", "50 per hour"],
+#     storage_uri="memory://",
+# )
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
@@ -54,28 +54,28 @@ mail = Mail(app)
 def serve(path):
     return send_from_directory(app.static_folder,'index.html')
 
-@app.route('/input',methods=["POST"])
-@limiter.limit("6/minute", override_defaults=False)
-def get_input():
-    qa_chain = get_chain(vectorstore)
-    data=request.get_json()
-    if len(data['question']) > 500:
-        return 'Question too long', 413
+# @app.route('/input',methods=["POST"])
+# @limiter.limit("6/minute", override_defaults=False)
+# def get_input():
+#     qa_chain = get_chain(vectorstore)
+#     data=request.get_json()
+#     if len(data['question']) > 500:
+#         return 'Question too long', 413
     
-    chat_history = []
-    i = 0
-    while i < len(data['chat_history']):
-        userMessage = data['chat_history'][i][1]
-        i+=1
-        botMessage = ""
-        while i < len(data['chat_history']) and data['chat_history'][i][0] == 'bot':
-            botMessage += data['chat_history'][i][1]+" "
-            i+=1
-        chat_history.append((userMessage,botMessage))
-    result = qa_chain({"question": data['question'], "chat_history": chat_history})
-    logToSheets = [datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),request.remote_addr,data['question'],result['answer']]
-    sheet.insert_row(logToSheets)
-    return {'message':result['answer']}
+#     chat_history = []
+#     i = 0
+#     while i < len(data['chat_history']):
+#         userMessage = data['chat_history'][i][1]
+#         i+=1
+#         botMessage = ""
+#         while i < len(data['chat_history']) and data['chat_history'][i][0] == 'bot':
+#             botMessage += data['chat_history'][i][1]+" "
+#             i+=1
+#         chat_history.append((userMessage,botMessage))
+#     result = qa_chain({"question": data['question'], "chat_history": chat_history})
+#     logToSheets = [datetime.now().strftime("%m/%d/%Y, %H:%M:%S"),request.remote_addr,data['question'],result['answer']]
+#     sheet.insert_row(logToSheets)
+#     return {'message':result['answer']}
     
 
 @app.route('/email',methods=["POST"])
